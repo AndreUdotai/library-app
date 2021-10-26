056edae5e82032d88860058f2e812d4fdff2c678
AndreUdotai <ubongudotai@gmail.com>
2021-10-26 23:12:04 +0100

    Wrote the book constructor function

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+      return `${title} by ${author}, ${pages} pages, ${read = read ? 'read' : 'not read yet'}`;
+    }
+}
+
+function addBookToLibrary(){
+    
+}
+
+  
